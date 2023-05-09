beae489967f25166cd63cf40f5ec1c599f22e19f
Фёдор Ситников <mr.prizrek@mail.ru>
2023-05-09 22:54:56 +0300

    Added Newton method

--- NewtonsMethod/newton.py
+++ NewtonsMethod/newton.py
@@ -8,8 +8,22 @@ def function(x):
 
 def function_gradient(x):
     x1, x2 = x
-    return [round(2 * x1 + x2 + 1, 4),
-            round(10 * x2 + x1, 4)]
+    return round(2 * x1 + x2 + 1, 4), round(10 * x2 + x1, 4)
+
+
+def hesse():
+    return [[2, 1], [1, 10]]
+
+
+def determinant(h):
+    return h[0][0] * h[1][1] - h[0][1] * h[1][0]
+
+
+def tr_hesse(h):
+    det = determinant(h)
+    matrix_tr = [[h[1][1], -h[0][1]],
+                 [-h[1][0], h[0][0]]]
+    return [[round(el[0] / det, 4), round(el[1] / det, 4)] for el in matrix_tr]
 
 
 def function_tk(x):
@@ -23,10 +37,10 @@ def function_tk(x):
     return t
 
 
-def steepest_gradient_descent_method(x, m=50, e1=1e-2, e2=105e-3):
+def newton_method(x, m=50, e1=1e-2, e2=105e-3):
     print(f'Функция: f(x) = x1^2 + 5x2^2 + x1x2 + x1')
     print(f'\tx0 = ({x[0]}, {x[1]})\n   \tM = {m}\n   \te1 = {e1}\n   \te2 = {e2}')
-    k, xk = 0, 0
+    k, xk, dk = 0, 0, 0
     while True:
         print(f'\nk = {k}')
         fg = function_gradient(x)
@@ -34,34 +48,46 @@ def steepest_gradient_descent_method(x, m=50, e1=1e-2, e2=105e-3):
 
         norma = round(math.sqrt(fg[0] ** 2 + fg[1] ** 2), 4)
         print(f'\tШаг 4. {norma} < {e1} ?')
-
         if norma < e1:
             print(f'x = {x}')
             return x, k
-        print(f'\tШаг 5. {k} >= {m} ?')
 
+        print(f'\tШаг 5. {k} >= {m} ?')
         if k >= m:
             print(f'x = {x}')
             return x, k
-        tk = function_tk(x)
-        print(f'\tШаг 6. {tk}')
 
-        xk = [round(x[0] - tk * fg[0], 4), round(x[1] - tk * fg[1], 4)]
-        print(f'\tШаг 7. {xk}')
+        h = hesse()
+        print(f'\tШаг 6. \t{h[0]}')
+        print(f'\t\t\t{h[1]}')
+
+        ht = tr_hesse(h)
+        print(f'\tШаг 7. \t{ht[0]}')
+        print(f'\t\t\t{ht[1]}')
+
+        det_ht = determinant(ht)
+        print(f'\tШаг 8. {det_ht} > 0 ?')
+        if det_ht > 0:
+            dk = [round(-el[0] * fg[0] - el[1] * fg[1], 4) for el in ht]
+            print(f'\tШаг 9. {dk}')
+        else:
+            dk = [-el for el in fg]
+
+        xk = [round(x[i] + dk[i], 4) for i in [0, 1]]
+        print(f'\tШаг 10. {xk}')
 
         f, s = round(math.sqrt((xk[0] - x[0]) ** 2 + (xk[1] - x[1]) ** 2), 4), \
             round(abs(function(xk) - function(x)), 4)
-        print(f'\tШаг 8. {f} < {e2} and {s} < {e2} ?')
+        print(f'\tШаг 11. {f} < {e2} and {s} < {e2} ?')
         if f < e2 and s < e2:
             print(f'\tx = {xk}')
             return xk, k
-
         x = xk
         k += 1
 
 
 x0 = [1, 1]
-method = steepest_gradient_descent_method(x0)
+method = newton_method(x0)
 print()
 print(f'Кол-во итераций: {method[1]}')
 print(f'Значение точки: x = {method[0]}')
